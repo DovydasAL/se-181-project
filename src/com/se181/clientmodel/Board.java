d3e8d624213baf8df497ab0555a0c492fb1c2e1b
DovydasAL <davidlazauskas1@gmail.com>
2020-08-13 12:49:16 -0400

    add function to check opponent moves

--- src/com/se181/clientmodel/Board.java
+++ src/com/se181/clientmodel/Board.java
@@ -1,5 +1,6 @@
 package com.se181.clientmodel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.se181.clientmodel.PieceColor.BLACK;
@@ -31,6 +32,42 @@ public class Board {
             }
         }
         return null;
+    }
+
+    public Board flipBoard() {
+        Board flippedBoard = new Board();
+        flippedBoard.whiteSet = this.whiteSet.shallowCopy();
+        flippedBoard.blackSet = this.blackSet.shallowCopy();
+        for (int i=0;i<flippedBoard.whiteSet.pieces.size();i++) {
+            ChessPiece piece = flippedBoard.whiteSet.pieces.get(i);
+            piece.position.row = (piece.position.row >= 4) ? (piece.position.row + 7) % 7 : 7 - piece.position.row;
+        }
+        for (int i=0;i<flippedBoard.blackSet.pieces.size();i++) {
+            ChessPiece piece = flippedBoard.blackSet.pieces.get(i);
+            piece.position.row = (piece.position.row >= 4) ? (piece.position.row + 7) % 7 : 7 - piece.position.row;
+        }
+        return flippedBoard;
+    }
+
+
+    public List<Square>calculateAllPossibleMove(PieceColor color){
+        List<Square> allValidMoves = new ArrayList<>();
+        PieceSet pieceSet;
+        if (color == WHITE)
+            pieceSet = this.whiteSet;
+        else
+            pieceSet = this.blackSet;
+        for (int i = 0; i < pieceSet.pieces.size(); i++) {
+            ChessPiece piece = pieceSet.pieces.get(i);
+            List<Square> validMoves = piece.validMoves(this);
+            allValidMoves.addAll(validMoves);
+        }
+        return allValidMoves;
+//        for (int i = 0; i < board.whiteSet.pieces.size(); i++) {
+//            ChessPiece piece = board.whiteSet.pieces.get(i);
+//            List<Square> validMoves = piece.validMoves(this.board);
+//            allValidMoves.add(validMoves);
+//        }
     }
 
     public ChessPiece getPieceAt(Square position, PieceColor color) {
