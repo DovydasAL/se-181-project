efae8a7af8a23b53bb58254e442fcf440b796107
EBAAD <eu37@drexel.edu>
2020-08-23 04:34:53 +0500

    adding serverTest

---
+++ src/Test/ServerTest.java
@@ -0,0 +1,21 @@
+import com.se181.Server.Server;
+import com.se181.Server.ServerThread;
+import com.se181.gui.MainForm;
+import org.junit.jupiter.api.Test;
+
+public class ServerTest {
+
+
+    @Test
+    void createServer(){
+        Server S = new Server();
+
+
+    }
+    @Test
+    void MainFormTest(){
+        MainForm M = new MainForm("chessTest");
+
+
+    }
+}
